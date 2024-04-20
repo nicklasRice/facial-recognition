ed78fe815b053ed0fd31fd9a57bc5dee2ae6605c
nicklasRice <nicklas.rice@ufl.edu>
2024-04-20 13:21:15 -0400

    CLI

---
+++ facial_recognition.py
@@ -0,0 +1,48 @@
+from train import Train
+from image_preprocessor import ImagePreprocessor
+from cv2 import face
+
+def eigen(args):
+    model = face.EigenFaceRecognizer.create()
+    common(args, model)
+
+
+def fisher(args):
+    model = face.FisherFaceRecognizer.create()
+    common(args, model)
+
+def lbph(args):
+    model = face.LBPHFaceRecognizer.create()
+    common(args, model)
+
+def common(args, model):
+    train = Train(args.data, model, ImagePreprocessor())
+    if (args.cross):
+        train.cross_validate()
+
+import argparse
+import pathlib
+
+parentParser = argparse.ArgumentParser(add_help=False)
+parentParser.add_argument('data', type=pathlib.Path, nargs='+', help='path to data')
+parentParser.add_argument('-s', '--split', type=float help='train-test split', default=0)
+parentParser.add_argument('-c', '--cross', help='cross validation', action='store_true', default=False)
+parentParser.add_argument('-ns', '--no-save', dest='save', help='do not save model', action='store_false',
+                          default=True)
+parentParser.add_argument('-m', '--metric', help='metric(s) to evaluate', nargs='+', choices=['accuracy'])
+parentParser.add_argument('-mo', '--model', type=pathlib.Path, help='path to saved model')
+
+parser = argparse.ArgumentParser(description='Facial recognition with OpenCV')
+subparsers = parser.add_subparsers(help='subcommands', required=True)
+
+parser_eigen = subparsers.add_parser('eigen', parents=[parentParser], description='Eigenfaces')
+parser_eigen.set_defaults(func=eigen)
+
+parser_fisher = subparsers.add_parser('fisher', parents=[parentParser], description='Fisherfaces')
+parser_fisher.set_defaults(func=fisher)
+
+parser_lbph = subparsers.add_parser('lbph', parents=[parentParser], description='LBPH')
+parser_lbph.set_defaults(func=lbph)
+
+args = parser.parse_args()
+args.func(args)
--- train.py
+++ train.py
@@ -1,31 +1,44 @@
-import pathlib
 import cv2 as cv
 
 class Train:
-    path = None
+    data_paths = []
+    model = None
     image_preprocessor = None
-    images = []
-    labels = []
-    label_names = []
+    train_x = []
+    train_y = []
+    test_x = []
+    test_y = []
 
 
-    def __init__(self, path, image_preprocessor=None) -> None:
-        self.path = pathlib.Path(path)
+    def __init__(self, paths, model, image_preprocessor, split) -> None:
+        self.data_paths = paths
         self.image_preprocessor = image_preprocessor
-        self._label_data()
+        self.model = model
+        images, labels = self._label_data()
+
         
 
-    def _label_data(self):
-        for i, f in enumerate(self.path.iterdir()):
-            if i == 10:
-                break
-            self.label_names.append(f.name)
-            for im in f.iterdir():
-                image = cv.imread(str(im))
-                if (self.image_preprocessor):
-                    image = self.image_preprocessor.process(image)
-                self.images.append(image)
-                self.labels.append(i)
+    def _label_data(self, split):
+        images = []
+        labels = []
+        name_to_label = {}
+        i = 0
+        for path in self.data_paths:
+            for f in path.iterdir():
+                if f.name not in name_to_label:
+                    name_to_label[f.name] = i
+                    i = i + 1
+                label = name_to_label[f.name]
+                for im in f.iterdir():
+                    image = cv.imread(str(im))
+                    if (self.image_preprocessor):
+                        image = self.image_preprocessor.process(image)
+                    self.images.append(image)
+                    self.labels.append(i)
+        return images, labels
+    
 
-    def cross_validate(self, model):
+    
+
+    def cross_validate(self):
         pass
